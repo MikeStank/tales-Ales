f38ae09d8550bc4c3775988025e71d2a0816aabb
LaBlanca Ponder <lablancaponder@gmail.com>
2018-03-22 14:50:21 -0600

    added config and routes folders and files

--- db/schemaAles.js
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE ales_db;
-USE ales_db;
-
-CREATE TABLE ales(
-id int AUTO_INCREMENT,
-PRIMARY KEY (id),
-drink_name varchar(50) NOT NULL,
-date TIMESTAMP);
--- db/schemaTales.js
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE tales_db;
-USE tales_db;
-
-CREATE TABLE tales(
-id int AUTO_INCREMENT,
-PRIMARY KEY (id),
-book_name varchar(50) NOT NULL,
-date TIMESTAMP);
