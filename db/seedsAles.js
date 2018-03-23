513c8625cc0f0a06d7095e43d108adafa8c606e6
Mike Stankiewicz <stankiem89@gmail.com>
2018-03-22 19:06:13 -0600

    added frontend stuff

--- db/seedsAles.js
+++
@@ -1,5 +0,0 @@
-INSERT INTO ales(drink_name) VALUES ('Mojito');
-INSERT INTO ales(drink_name) VALUES ('Manhattan');
-INSERT INTO ales(drink_name) VALUES ('Long Island Iced Tea');
-INSERT INTO ales(drink_name) VALUES ('Whiskey Sour');
-INSERT INTO ales(drink_name) VALUES ('Martini');
--- db/seedsTales.js
+++
@@ -1,5 +0,0 @@
-INSERT INTO tales(book_name) VALUES ('East of Eden');
-INSERT INTO tales(book_name) VALUES ('Oil!');
-INSERT INTO tales(book_name) VALUES ('50 Shades of Grey');
-INSERT INTO tales(book_name) VALUES ('Moby Dick');
-INSERT INTO tales(book_name) VALUES ('The Heart of Darkness');
