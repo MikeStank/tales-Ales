7804f68f08d3db6028582397dfd2f6a8a023f5d6
LaBlanca Ponder <31378269+lponder82@users.noreply.github.com>
2018-03-25 11:18:23 -0600

    Add files via upload
    changed some lines in index file  just to test search out... changed the id names.  also added a main.js

---
+++ main.js
@@ -0,0 +1,36 @@
+function bookSearch() {
+    console.log("this function runs");
+
+    var search = document.getElementById("search").value;
+    document.getElementById("results").innerHTML = "";
+    
+    console.log(search);
+
+    $.ajax({
+
+        url: "https://www.googleapis.com/books/v1/volumes?q=" + search,        
+        dataType: "json",
+        success: function(data) {
+
+            for (var i = 0; i < 1; i++) {
+
+                results.innerHTML += "<a href='match.html'>" + "<h2>" + data.items[i].volumeInfo.title + "</h2>" + "</a>";
+                results.innerHTML += "<h3> By " + data.items[i].volumeInfo.authors + "</h3>";
+                results.innerHTML += "<h3> Description </h3>" + data.items[i].volumeInfo.description + "<br>";
+                results.innerHTML += "<img src=" + data.items[i].volumeInfo.imageLinks.thumbnail +">";
+                results.innerHTML += data.items[i].volumeInfo.categories;
+            }
+            
+        },
+        type: "GET"
+    });
+}
+
+document.getElementById("button").addEventListener("click", bookSearch, false);
+   
+
+// AIzaSyA70Eil2HN_JenQ29_DR9G2OprJ7hqbsNQ
+
+
+// key: 4BcodGUNTcNv22tOHLgOtg
+// secret: tnuxltzRMxfDfbPQRP1Hc1J1BiqcUW71Otn7RY9ySc
